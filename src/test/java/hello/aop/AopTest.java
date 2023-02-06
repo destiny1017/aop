2c74bd3017bfee7064dd4ac6f5d735576ed54b85
KDH_Notebook <destiny1017@nate.com>
2023-02-06 18:46:39 +0900

    Spring AOP구현 test2

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -2,9 +2,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV1;
-import hello.aop.order.aop.AspectV2;
-import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -15,12 +13,14 @@ import org.springframework.context.annotation.Import;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
-@Import(AspectV3.class)
+//@Import({AspectV5Order.TxAspect.class, AspectV5Order.LogAspect.class})
+@Import(AspectV6Advice.class)
 @SpringBootTest
 public class AopTest {
 
     @Autowired
     OrderService orderService;
+
     @Autowired
     OrderRepository orderRepository;
 
